4b07d2e23938289343b4f342155a51f6821458e7
uillasnr <wiliasreis@hotmail.com>
2025-01-01 21:14:22 -0300

    feat: types category

---
+++ src/types/category.ts
@@ -0,0 +1,16 @@
+export type CategoryProps = {
+  _id: string;
+  userId: string;
+  title: string;
+  Icon?: string; 
+  color: string;
+};
+
+  export interface Category {
+    _id?: string;
+    userId: string;
+    title: string;
+    Icon?: string;
+    color: string;
+  }
+  
