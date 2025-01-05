8841e526785471252c985b2000136835e5375609
uillasnr <wiliasreis@hotmail.com>
2025-01-05 20:44:36 -0300

    feta: add library victory-native

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'], // Corrigido para 'nativewind/babel' sem 'tailwindcss-'
+  plugins: ['nativewind/babel', "react-native-reanimated/plugin"], // Corrigido para 'nativewind/babel' sem 'tailwindcss-'
   };
 };
