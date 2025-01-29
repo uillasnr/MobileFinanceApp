5b8b160299b2b8828edd12911b48514d3b92fcf5
uillasnr <wiliasreis@hotmail.com>
2025-01-28 22:32:57 -0300

    feat: add componente color picker

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -9,13 +9,7 @@ import {
   Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
-
-const colorOptions = [
-  "#FF5733", "#33FF57", "#3357FF", "#FF33FF", "#FFFF33", "#33FFFF", "#FF8033", "#8033FF",
-  "#33FF99", "#33CCFF", "#FFCC33", "#9966FF", "#66FF33", "#FF9933", "#CC33FF", "#FFD700",
-  "#D2691E", "#8A2BE2", "#DC143C", "#20B2AA"
-];
+import colorOptions from "../utils/colorOptions";
 
 interface ColorPickerProps {
   selectedColor: string;
@@ -24,7 +18,11 @@ interface ColorPickerProps {
 
 const ColorPicker = ({ selectedColor, onColorSelect }: ColorPickerProps) => {
   const [isVisible, setIsVisible] = useState(false);
-  const [numColumns, setNumColumns] = useState(5); 
+
+  const screenWidth = Dimensions.get("window").width;
+  const colorSize = screenWidth * 0.1;
+  const colorSpacing = screenWidth * 0.02;
+  const numColumns = Math.floor(screenWidth / (colorSize + colorSpacing));
 
   const handleSelectColor = (color: string) => {
     onColorSelect(color);
@@ -33,35 +31,28 @@ const ColorPicker = ({ selectedColor, onColorSelect }: ColorPickerProps) => {
 
   return (
     <>
-      <View className="flex-row justify-between items-center pb-4 border-b border-gray-500 w-full">
+      <View className="flex-row justify-between items-center py-3 border-b  border-gray-500 w-full">
         <View className="flex-row items-center">
           <Ionicons name="color-palette-outline" size={25} color="#E2E8F0" />
-          <Text className="pl-5 text-[#9CA3AF]">{"Escolha uma cor"}</Text>
+          <Text className="pl-3 text-[#9CA3AF]">Escolha uma Cor</Text>
         </View>
 
         <TouchableOpacity
           onPress={() => setIsVisible(true)}
           className="flex-row items-center gap-3"
         >
-          <View
-            className={`w-6 h-6 rounded-full flex items-center justify-center ${
-              selectedColor ? "" : "border border-gray-400"
-            }`}
-          >
+          <View className="w-10 h-10 rounded-full flex items-center justify-center border-2 border-[#9CA3AF]">
             <View
-              className={`w-5 h-5 rounded-full ${selectedColor ? "" : ""}`}
+              className="w-6 h-6 rounded-full"
               style={{ backgroundColor: selectedColor || "transparent" }}
             />
           </View>
-
-          {/* "Outras cores" text */}
           <View className="bg-gray-500 rounded-lg flex px-4 justify-center items-center text-center h-6">
-            <Text className="text-gray-200">{"Outras cores"}</Text>
+            <Text className="text-gray-200">Outras ...</Text>
           </View>
         </TouchableOpacity>
       </View>
 
-      {/* Color Picker Modal */}
       <Modal transparent visible={isVisible} animationType="fade">
         <TouchableWithoutFeedback onPress={() => setIsVisible(false)}>
           <View
@@ -76,30 +67,27 @@ const ColorPicker = ({ selectedColor, onColorSelect }: ColorPickerProps) => {
               style={{
                 backgroundColor: "#334155",
                 borderRadius: 12,
-                padding: 15,
-                width: Dimensions.get("window").width - 40,
+                padding: 12,
+                width: screenWidth - 40,
+                maxHeight: Dimensions.get("window").height * 0.8,
               }}
             >
               <FlatList
-                key={numColumns} 
                 data={colorOptions}
                 keyExtractor={(item) => item}
+                key={numColumns.toString()}
                 numColumns={numColumns}
                 renderItem={({ item }) => (
                   <TouchableOpacity
                     onPress={() => handleSelectColor(item)}
                     style={{
+                      width: colorSize,
+                      height: colorSize,
+                      margin: colorSpacing / 2,
+                      borderRadius: colorSize / 2,
                       backgroundColor: item,
-                      width: 50,
-                      height: 50,
-                      margin: 8,
-                      borderRadius: 25,
-                      borderWidth: item === selectedColor ? 4 : 1,
                       borderColor: item === selectedColor ? "#FFFFFF" : "#333",
-                      shadowColor: item === selectedColor ? "#FFFFFF" : "transparent",
-                      shadowOpacity: 0.8,
-                      shadowRadius: 10,
-                      elevation: 5,
+                      borderWidth: item === selectedColor ? 4 : 1,
                     }}
                   />
                 )}
