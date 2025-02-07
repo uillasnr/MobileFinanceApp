26a9ce103707a1abd35b9fb8f28b534531df3746
uillasnr <wiliasreis@hotmail.com>
2025-02-06 21:47:18 -0300

    feat: add login screen

---
+++ src/app/(public)/login.tsx
@@ -0,0 +1,149 @@
+import { Link, useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/src/Validators/schemas";
+import { Auth } from "@/src/types/auth";
+
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+  const {
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<Auth>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function handleLogin(data: Auth) {
+    try {
+      console.log("Login data:", data);
+      router.push("/(tabs)/home");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <SafeAreaView className="flex-1 bg-background-dark">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex-1"
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View className="min-h-full justify-center items-center px-6 py-8">
+            {/* Header Section */}
+            <View className="items-center mb-6 ">
+              <Image
+                source={require("../../../assets/images/logo.png")}
+                className="w-36 h-36"
+                resizeMode="cover"
+              />
+              <Text className="text-2xl font-bold text-text-light mt-2">
+                FinanceApp
+              </Text>
+              <Text className="text-sm text-text-light mt-1">
+                Acesse sua conta
+              </Text>
+            </View>
+
+            {/* Form Container */}
+            <View className="w-full max-w-md p-2">
+              {/* Email Input */}
+              <View className="mb-6">
+                <TextInput
+                  className="w-full bg-card-dark border h-14 rounded-xl border-gray-500 text-text-light pl-3"
+                  placeholder="E-mail"
+                  placeholderTextColor="#A1A1AA"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={(text) => setValue("email", text)}
+                />
+                {errors.email && (
+                  <Text className="text-red-500 text-sm mt-1 pl-1">
+                    {errors.email.message}
+                  </Text>
+                )}
+              </View>
+
+              {/* Password Input */}
+              <View className="mb-6">
+                <View className="relative">
+                  <TextInput
+                    className="w-full bg-card-dark border h-14 rounded-xl border-gray-500 text-text-light pl-3 pr-12"
+                    placeholder="Senha"
+                    placeholderTextColor="#A1A1AA"
+                    secureTextEntry={!showPassword}
+                    onChangeText={(text) => setValue("password", text)}
+                  />
+                  <TouchableOpacity
+                    className="absolute right-3 h-full justify-center"
+                    onPress={() => setShowPassword((prev) => !prev)}
+                  >
+                    <Ionicons
+                      name={showPassword ? "eye" : "eye-off"}
+                      size={22}
+                      color="#9CA3AF"
+                    />
+                  </TouchableOpacity>
+                </View>
+                {errors.password && (
+                  <Text className="text-red-500 text-sm mt-1 pl-1">
+                    {errors.password.message}
+                  </Text>
+                )}
+              </View>
+
+              {/* Login Button */}
+              <TouchableOpacity
+                onPress={handleSubmit(handleLogin)}
+                className="bg-[#6200ee] p-3 rounded-xl shadow-sm mb-4"
+              >
+                <Text className="text-white text-center text-lg font-semibold">
+                  Entrar
+                </Text>
+              </TouchableOpacity>
+
+              {/* Register Link */}
+              <Link href="/register" asChild>
+                <Pressable>
+                  <Text className="text-center text-gray-500">
+                    Não tem uma conta? <Text className="font-bold">Cadastre-se</Text>
+                  </Text>
+                </Pressable>
+              </Link>
+            </View>
+
+            <Text className="text-gray-500 text-xs text-center mt-8 px-6">
+          Seus dados financeiros estão protegidos com criptografia de ponta a ponta
+        </Text>
+      
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
