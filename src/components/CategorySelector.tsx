f061d2b7a821cda5e48764b88be15cb2843d8c85
uillasnr <wiliasreis@hotmail.com>
2025-02-12 22:02:56 -0300

    feat: add componente CategorySelector

--- src/components/CategorySelector.tsx
+++ src/components/CategorySelector.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import {
   View,
   Text,
@@ -6,21 +6,38 @@ import {
   TouchableWithoutFeedback,
   Dimensions,
 } from "react-native";
-import BottomSheet, { BottomSheetFlatList, BottomSheetView } from "@gorhom/bottom-sheet";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import BottomSheet, {
+  BottomSheetFlatList,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+import NewCategoryCreator from "./NewCategorySelector";
+
+
 
 interface CategorySelectorProps {
   categories: string[];
   onSelectCategory: (category: string) => void;
+  onCreateCategory: (category: { name: string; color: string }) => void;
   isVisible: boolean;
   onClose: () => void;
   selectedCategory?: string;
+  type: "expense" | "income";
 }
 
-export default function CategorySelector({ categories, onSelectCategory, isVisible,
-   onClose, selectedCategory,}: CategorySelectorProps) {
+export default function CategorySelector({
+  categories,
+  onSelectCategory,
+  onCreateCategory,
+  isVisible,
+  onClose,
+  selectedCategory,
+  type,
+}: CategorySelectorProps) {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const screenHeight = Dimensions.get("window").height;
+
+  const [isCreatingCategory, setIsCreatingCategory] = useState(false);
 
   const handleSelectCategory = useCallback(
     (category: string) => {
@@ -52,7 +69,9 @@ export default function CategorySelector({ categories, onSelectCategory, isVisib
 
           <View
             className={`w-6 h-6 rounded-full flex items-center justify-center ${
-              isSelected ? "border-2 border-secondary" : "border border-gray-400"
+              isSelected
+                ? "border-2 border-secondary"
+                : "border border-gray-400"
             }`}
           >
             <View
@@ -72,56 +91,85 @@ export default function CategorySelector({ categories, onSelectCategory, isVisib
   }
 
   return (
-    <TouchableWithoutFeedback onPress={onClose} accessible={false}>
-      <View style={{ 
-        flex: 1, 
-        backgroundColor: "rgba(0, 0, 0, 0.7)",
-        height: screenHeight,
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-      }}>
-        <BottomSheet
-          containerStyle={{ 
-            flex: 1, 
-            backgroundColor: "transparent",
-          }}
-          index={0}
-          snapPoints={["50%"]} 
-          onClose={onClose}
-          ref={bottomSheetRef}
-          backgroundStyle={{
-            backgroundColor: "#334155",
-          }}
-          enableOverDrag
-          enablePanDownToClose
-          handleIndicatorStyle={{
-            backgroundColor: "#9CA3AF",
+    <>
+      <TouchableWithoutFeedback onPress={onClose} accessible={false}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+            height: screenHeight,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
           }}
         >
-          <BottomSheetView style={{ flex: 1, }}>
-            <View className="p-4 flex-1 ">
-              <View className="flex-row justify-between items-center mb-4">
-                <Text className="text-xl font-medium text-center text-gray-200 flex-1">
-                  Selecione uma Categoria
-                </Text>
-               
-              </View>
+          <BottomSheet
+            containerStyle={{
+              flex: 1,
+              backgroundColor: "transparent",
+            }}
+            index={0}
+            snapPoints={["50%"]}
+            onClose={onClose}
+            ref={bottomSheetRef}
+            backgroundStyle={{
+              backgroundColor: "#334155",
+            }}
+            enableOverDrag
+            enablePanDownToClose
+            handleIndicatorStyle={{
+              backgroundColor: "#9CA3AF",
+            }}
+          >
+            <BottomSheetView style={{ flex: 1 }}>
+              <View className="p-4 flex-1">
+                <View className="flex-row justify-between items-center mb-4">
+                  <Text className="text-xl font-medium text-center text-gray-200 flex-1">
+                    Selecione uma Categoria
+                  </Text>
+                </View>
 
-              <BottomSheetFlatList
-                data={categories}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={renderItem}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 20,}}
-                
-              />
-            </View>
-          </BottomSheetView>
-        </BottomSheet>
-      </View>
-    </TouchableWithoutFeedback>
+                {/* Button to Create New Category */}
+                <View className=" border-b border-gray-600 ">
+                  <TouchableOpacity
+                    onPress={() => setIsCreatingCategory(true)}
+                    className="flex-row items-center p-3"
+                  >
+                    <View className="border-2 bg-gray-600 border-gray-600 w-8 h-8 rounded-full flex items-center justify-center">
+                      <AntDesign name="plus" size={20} color="#FFFFFF" />
+                    </View>
+
+                    <Text className="text-white text-center ml-2 font-medium">
+                      Criar Nova Categoria
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+
+                <BottomSheetFlatList
+                  data={categories}
+                  keyExtractor={(item, index) => index.toString()}
+                  renderItem={renderItem}
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{ paddingBottom: 20 }}
+                />
+              </View>
+            </BottomSheetView>
+          </BottomSheet>
+        </View>
+      </TouchableWithoutFeedback>
+
+      {/* Modal para criação de nova categoria */}
+      <NewCategoryCreator
+        onCreateCategory={(category) => {
+          onCreateCategory(category);
+          setIsCreatingCategory(false);
+        }}
+        isVisible={isCreatingCategory}
+        onClose={() => setIsCreatingCategory(false)}
+        type={type}
+      />
+    </>
   );
 }
