c92f711999ddddf9baa9fdfcfe0a837108a8db8c
uillasnr <wiliasreis@hotmail.com>
2025-04-09 20:43:03 -0300

    fix: correction of the category in the car

--- src/components/transactionCard.tsx
+++ src/components/transactionCard.tsx
@@ -8,9 +8,11 @@ export default function TransactionCard({
   title,
   category,
   amount,
+  type,
 }: TransactionCardProps) {
-  const amountColor = amount < 0 ? "bg-red-500" : "bg-green-500"; 
-  const TextColor = amount < 0 ?  "text-red-500" : "text-green-500"; 
+  const isExpense = type === "expense"; // Verifica se é despesa
+  const amountColor = isExpense ? "bg-red-500" : "bg-green-500";
+  const TextColor = isExpense ? "text-red-500" : "text-green-500";
 
   return (
     <View className="bg-background-dark p-2 mb-2 px-3 rounded-lg shadow-md flex-row items-center justify-between">
@@ -21,22 +23,24 @@ export default function TransactionCard({
           className={`w-8 h-8 rounded-full flex items-center justify-center ${amountColor}`}
         >
           <AntDesign name="pushpino" size={16} color="#E2E8F0" />
+        {/*   <Text className="text-gray-500 text-xs">{category.Icon}</Text> */}
         </View>
 
         {/* Informações */}
         <View>
-          <Text className="text-text-light font-bold text-sm">{title}</Text>
-          <Text className="text-gray-500 text-xs">{category}</Text>
+          <Text className="text-text-light font-bold text-sm">
+            {title.length > 20 ? title.slice(0, 25) + "..." : title}
+          </Text>
+          <Text className="text-gray-500 text-xs">{category.title}</Text>
         </View>
       </View>
 
       {/* Valor */}
       <View>
         <Text className={`text-base font-bold text-right ${TextColor}`}>
-          {amount < 0
-            ? `-${formatCurrency(Math.abs(amount))}`
-            : `${formatCurrency(amount)}`}
+          {formatCurrency(amount)}
         </Text>
+
         <View className="flex-row items-center gap-2 justify-end pr-1">
           <AntDesign name="pushpino" size={13} color="#E2E8F0" />
           <SimpleLineIcons name="envelope-letter" size={13} color="#E2E8F0" />
